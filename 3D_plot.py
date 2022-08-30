88ed87f11040609657c1d1c28fd3c46435f95521
imran zaheer <izaheer612@gmail.com>
2022-08-31 01:25:11 +0500

    intial commit

---
+++ 3D_plot.py
@@ -0,0 +1,146 @@
+import cv2
+import matplotlib.pyplot as plt
+import glob
+cv_img = []
+
+"""
+Reading filtered images and saving
+in 'cv_img' array
+"""
+for i in range(1,17):
+  for img in glob.glob(f"output/crop{i}.png"):
+    n= cv2.imread(img)
+    cv_img.append(n)
+
+
+z_cordinate=[]
+x_cordinate=[]
+for i in range(0,len(cv_img)):
+  h,w,_=cv_img[i].shape
+  z=[]
+  x=[]
+  
+  """
+  Iterating each pixel and appending locations where edge exits
+  """
+  for l in range(0,h):
+    for j in range(0,w):
+      b,g,r=cv_img[i][l,j]
+      if (b,g,r)!=(0,0,0):
+        z.append(j)
+        x.append(l)
+  #z.append(h)
+  #x.append(w)
+  z_cordinate.append(z)
+  x_cordinate.append(x)
+
+
+s=1.5
+y_axis=[]
+# Y-axis given range 16 for 16 croped imgs in "output/"
+# for only 3 imgs in crop set range(1, 3)
+
+# for i in range(1, 3):
+for i in range(1,17):
+  y_axis.append(s)
+  s=s+1.5
+
+# y_cordinates = []
+
+# for i in range(len(x_cordinate)):
+#   y = []
+#   for j in range(len(x_cordinate[i])):
+#     # y_coordinates[i] = [[x*y_axis[i] for x in y] for y in  x_cordinate[i]]
+#     y.append(y_axis[i] * x_cordinate[i][j])
+
+#   y_cordinates.append(y)
+
+
+import math
+import numpy as np
+
+
+# def x_cor(x, y, theta):
+#   return round((x * np.cos( math.radians(theta) ))  + (y * np.sin( math.radians(theta) )), 3)
+
+# def y_cor(x, y, theta):
+#   return round(( -(x) * np.sin( math.radians(theta) ))  + (y * np.cos( math.radians(theta) )), 3)
+
+
+
+# for i in range(len(x_cordinate)):
+#   for j in range(len(x_cordinate[i])):
+#     # x_cordinate[i][j] = x_cor(x_cordinate[i][j], y_cordinates[i][j], i * 11)
+#     y_cordinates[i][j] = y_cor(x_cordinate[i][j], y_cordinates[i][j], i * 11)
+#     # y_coordinates[i] = [[x*y_axis[i] for x in y] for y in  x_cordinate[i]]
+#     # y.append(y_axis[i] * x_cordinate[i][j])
+
+
+
+
+
+# importing mplot3d toolkits
+from mpl_toolkits import mplot3d
+# import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+# syntax for 3-D projection
+ax = plt.axes(projection ='3d')
+
+# defining axes
+test=[]
+
+print(f'length of y_axis: {len(y_axis)}')
+print(y_axis)
+# print(y_cordinates[1])
+print(f'length of x_axis: {len(x_cordinate)}')
+# print(x_cordinate[1])
+print(f'length of z_axis: {len(z_cordinate)}')
+# print(z_cordinate[1])
+
+for i in range(0,len(y_axis)):
+  print("list index",  str(i))
+  ax.scatter(x_cordinate[i], y_axis[i], z_cordinate[i],  color='red',s=1)
+  # ax.scatter(x_cordinate[i], y_cordinates[i], z_cordinate[i], color='red',s=1)
+
+  for k in range(len(x_cordinate[i])):
+    test.append([x_cordinate[i][k],y_axis[i], z_cordinate[i][k]])
+    # test.append([x_cordinate[i][k], z_cordinate[i][k], y_cordinates[i][k]])
+
+
+# syntax for plotting
+ax.set_title('3d Scatter conical flask') 
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+ax.view_init(30, -60)
+plt.show()
+
+'''cor=[]
+for i in range(len(y_axis)):
+  for k in range(len(x_cordinate[i])):
+    sub=[]
+    sub.append(x_cordinate[i][k])
+    sub.append(y_axis[i])
+    sub.append(x_cordinate[i][k])
+    cor.append(sub)'''
+# Pass numpy array to Open3D.o3d.geometry.PointCloud and visualize
+import open3d as o3d
+#xyz = np.random.rand(100, 3)
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(test)
+o3d.io.write_point_cloud('data2.ply', pcd)
+o3d.visualization.draw_geometries([pcd])
+
+import numpy as np
+import pyvista as pv
+
+# points is a 3D numpy array (n_points, 3) coordinates of a sphere
+cloud = pv.PolyData(test)
+#cloud.plot()
+
+volume = cloud.delaunay_3d(alpha=3.)
+shell = volume.extract_geometry()
+shell.plot()
---
+++ README.md
@@ -0,0 +1,60 @@
+# Towards 3D Image Reconstruction: 3D Visualization
+Three dimensional (3D) ultrasound image reconstruction from two dimensional (2D) images
+is a suitable method for analyzing some anatomy related abnormalities.Ultrasound image
+reconstruction system is required in order to view the specific part of an object so that
+it can be used for analysis purposes. Cost of a 3D Ultrasound machine is high and un
+affordable of lower income hospitals. Here we are exploring methods to convert 2D US
+images to 3D using basic image processing techniques, K mean clustering and marching
+cubes algorithm.
+
+# Data
+The objective of this project is to convert 2 D slices of ultrasound image to 3D. For this purpose we require ultrasound images.Ultrasound images of a conical frustum is taken using a linear ultrasound probe. There are 16 images (only 3 images are publically posted in this repo as a sample), those are taken as data for this project.Few such images are shown below.
+
+
+![uS](https://user-images.githubusercontent.com/85213549/149622467-5ff5004b-c6c7-40ad-bcb6-7eef933aa9c7.jpeg)
+
+
+# Pre-processing
+## K Means clustering
+K means clustering is an unsupervised algorithm used to identify clusters in the data. K
+means clustering can be used in image data to segment interesting areas from the back-
+ground. It clusters given data into K clusters using the k centroids.This algorithm is used
+when we have unlabelled data. The goal is to find certain groups based on some kind of
+similarity in the data with the number of groups represented by K.
+
+![K Means](https://user-images.githubusercontent.com/85213549/149622520-381d1dcc-3008-40b2-8ba0-24e8a03e1a09.png)
+
+
+## Canny edge detection
+OpenCV provides cv2.Canny(image, threshold1,threshold2) function for edge detec-
+tion.The first argument is our input image. Second and third arguments are our min and
+max threshold respectively. Using the Canny algorithm, the function discovers edges in
+the input image (8-bit input picture) and marks them in the output map edges. For edge
+linking, the least value between threshold1 and threshold2 is chosen. The biggest value is
+used to locate the beginnings of strong edge segments.
+
+![canny](https://user-images.githubusercontent.com/85213549/149622526-cefb26df-ff8a-404c-a182-c5cb7840d57a.png)
+# Output
+## Open3D output
+
+![Screenshot from 2022-01-05 17-41-32](https://user-images.githubusercontent.com/85213549/149622575-dee531e2-c198-4703-bc7d-7eb53c9b5862.png)
+## pyvista Output
+
+![Screenshot from 2022-01-05 18-35-05](https://user-images.githubusercontent.com/85213549/149622613-ccd204c7-3e29-41fe-9737-798c11fe5fbd.png)
+
+
+# How to run the code
+
+1. Clone the repository.
+2. Go inside the folder 
+3. Install requirements using command ```pip install requirements```
+4. Run the code 3D_plot.py in python 3 , ```python3 3D_plot.py```
+
+# About code base
+
+1. The ultrasound image data are stored in folder "data" in png format
+2. The filtered images are stored in folder "output"
+3. filter.py filter US images
+4. 3D_plot.py convert 2D into 3D
+5. data2.py is a point cloud file formed by running 3D_plot.py
+6. dummy.py run 3D reconstruction on dummy images
